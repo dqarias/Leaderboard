12121d1baac2b7a5130dc4410b0af8dcf7c743fb
dqarias <dino.quispea@gmail.com>
2022-06-15 15:58:20 -0500

    Update render recent scores dynamically

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-import './style.css';
-import printMe from './print.js';
+import "./style.css";
 
-document.getElementById('btn-print').addEventListener('click', printMe);
+const result = [
+  {
+    user: "John Doe",
+    score: 42,
+  },
+  {
+    user: "Peter Parker",
+    score: 35,
+  },
+  {
+    user: "Wonder Woman",
+    score: 50,
+  },
+];
+
+const scores = document.querySelector(".scores__body");
+
+result.forEach((element) => {
+  scores.innerHTML += `<li class="scores__list">${element.user} : ${element.score}</p></li>`;
+});
