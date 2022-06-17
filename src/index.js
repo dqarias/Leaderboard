354916ab414a8eab9418ad45ad6f43085251d327
dqarias <dino.quispea@gmail.com>
2022-06-17 00:40:53 -0500

    Update index.js file to add score and render scores

--- src/index.js
+++ src/index.js
@@ -1,22 +1,32 @@
 import './style.css';
+import addNewScoreUser from './modules/addScoreUser'
+import getScoreUsers from './modules/getScoreUsers'
 
-const result = [
-  {
-    user: 'John Doe',
-    score: 42,
-  },
-  {
-    user: 'Peter Parker',
-    score: 35,
-  },
-  {
-    user: 'Wonder Woman',
-    score: 50,
-  },
-];
+const btnAddUser = document.getElementById('add_score')
+const btnRefreshScore = document.getElementById('refresh')
+const scoresUsers = document.querySelector('.scores__body');
+const inputUser = document.getElementById('name')
+const inputScore = document.getElementById('score')
 
-const scores = document.querySelector('.scores__body');
+const addScore = () =>{
+  if(inputUser.value && inputScore.value){
+    addNewScoreUser(inputUser.value, Number(inputScore.value) )
+  }
+  inputUser.value = '';
+  inputScore.value = '';
+}
 
-result.forEach((element) => {
-  scores.innerHTML += `<li class="scores__list">${element.user} : ${element.score}</p></li>`;
-});
+const renderScores = () => {
+  const userScore = getScoreUsers();
+  console.log("userscore", userScore)
+  scoresUsers.innerHTML='';
+  userScore.then((scores)=>{
+    scores.sort((a, b) => b.score - a.score);
+    scores.forEach((element) => {
+      scoresUsers.innerHTML += `<li class="scores__list">${element.user} : ${element.score}</p></li>`;
+    });
+  })
+}
+
+btnAddUser.addEventListener('click', addScore)
+btnRefreshScore.addEventListener('click', renderScores)
